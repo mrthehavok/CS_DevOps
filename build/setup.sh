e82d865ffe8ceaab673f2769a5de6feca71a9852
mrthehavok <havok16121989@gmail.com>
2021-11-18 16:13:36 +0300

    change service

--- build/setup.sh
+++ build/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "Enable autostart"
-cp /tmp/web_flask.service /etc/systemd/system/web_flask/web_flask.service
+cp /tmp/web_flask.service /etc/systemd/system/web_flask.service
 cp /tmp/application.py /home/ec2-user/web_flask/application.py
 cp /tmp/index.html /home/ec2-user/web_flask/templates/index.html
+systemctl start web_flask
+systemctl status web_flask
 systemctl enable web_flask
