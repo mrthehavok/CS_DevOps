322e6ec5de0783c15445ea08ad9a18a079ee1405
mrthehavok <havok16121989@gmail.com>
2021-11-18 16:02:38 +0300

    update path

--- build/setup.sh
+++ build/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "Enable autostart"
-sudo cp /tmp/web_flask.service /etc/systemd/system/web_flask/web_flask.service
-sudo systemctl enable web_flask
+cp /tmp/web_flask.service /etc/systemd/system/web_flask/web_flask.service
+cp /tmp/application.py /home/ec2-user/web_flask/application.py
+cp /tmp/index.html /home/ec2-user/web_flask/templates/index.html
+systemctl enable web_flask
