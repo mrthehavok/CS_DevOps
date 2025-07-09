3960eaca698e0259b0ee89af0300bdbcb3e40d65
mrthehavok <89513597+mrthehavok@users.noreply.github.com>
2025-07-09 15:48:00 +0200

    Fix pre-setup script user

--- build/presetup.sh
+++ build/presetup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "Install packages"
-yum install python3 -y
-su - ec2-users
-pip3 install boto3 
-pip3 install flask 
+yum install python3 -ypip3 install boto3
+pip3 install flask
 mkdir -p /home/ec2-user/web_flask/templates
