223756789a63ca631b3e85709a960d5a111ece3a
mrthehavok <89513597+mrthehavok@users.noreply.github.com>
2025-07-09 15:52:51 +0200

    fix username in presetup script

--- build/presetup.sh
+++ build/presetup.sh
@@ -2,7 +2,6 @@
 
 echo "Install packages"
 yum install python3 -y
-su - ec2-users
+su - ec2-user
 pip3 install boto3 
-pip3 install flask 
-mkdir -p /home/ec2-user/web_flask/templates
+pip3 install flask mkdir -p /home/ec2-user/web_flask/templates
